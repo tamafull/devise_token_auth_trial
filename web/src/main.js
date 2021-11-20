01c0327ab6811c0a5ed9598b668b91880b2f087a
tamafull <tamastatic@gmail.com>
2021-11-20 19:03:44 +0900

    axios-settings

--- web/src/main.js
+++ web/src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 
 createApp(App).mount('#app')
+
+if (process.env.NODE_ENV === 'production') {
+  // TODO: Change after initial deploy.
+  axios.defaults.baseURL = 'http://localhost:3000';
+} else {
+  axios.defaults.baseURL = 'http://localhost:3000';
+}
