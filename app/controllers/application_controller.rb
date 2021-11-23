ef6e133040421e3f1f629f354385afc16cb7b0cd
tamafull <tamastatic@gmail.com>
2021-11-23 21:19:26 +0900

    ユーザー認証追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :authenticate_api_v1_user!, unless: :devise_controller? # TODO: authenticate_user!にしたい
   skip_before_action :verify_authenticity_token, if: :devise_controller?
 end
