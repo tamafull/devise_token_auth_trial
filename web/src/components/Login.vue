d5f9fc197a4d27c3d19f1837d91085a9ae835394
tamafull <tamastatic@gmail.com>
2021-11-23 10:16:31 +0900

    ログイン画面

---
+++ web/src/components/Login.vue
@@ -0,0 +1,39 @@
+<template>
+  <form @submit.prevent="login">
+    <span>メールアドレス:</span>
+    <input v-model="email.data"><br>
+    <span>パスワード:</span>
+    <input v-model="password.data"><br>
+    <button type="submit">ログイン</button><br>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Login',
+  data: function () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login: function () {
+      axios
+        .post(`/api/v1/auth/sign_in`, {
+          email: this.email.data,
+          password: this.password.data
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
