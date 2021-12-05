72c0eb8b040afc8687f65608898ba08ea093ecf6
tamafull <tamastatic@gmail.com>
2021-12-05 11:34:13 +0900

    Revert "deviseのhelperメソッドで付加されるnamespaceを除去"
    This reverts commit b5f75639102c2437c3ffd505c4b43d7d69ffdb03.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
-  devise_group :user, contains: [:api_v1_user]
-  before_action :authenticate_user!, unless: :devise_controller?
+  before_action :authenticate_api_v1_user!, unless: :devise_controller? # TODO: authenticate_user!にしたい
   skip_before_action :verify_authenticity_token, if: :devise_controller?
 end
