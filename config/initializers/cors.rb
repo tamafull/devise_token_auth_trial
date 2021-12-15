7de84715ebf9f12e39ceb7c0a2c57cce8f9f1118
tamafull <tamastatic@gmail.com>
2021-12-15 21:52:58 +0900

    GitHub Pages用のCORS設定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'http://localhost:8080'
+    origins 'http://localhost:3000', 'http://localhost:8080', 'https://tamafull.github.io'
 
     resource '*',
       headers: :any,
